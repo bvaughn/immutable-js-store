c86425e1141d8d9f4fa9bbde5d7e5c756df9c39e
Brian Vaughn <brian.david.vaughn@gmail.com>
2016-06-05 23:34:01 -0700

    Build fixes

--- webpack.config.dist.js
+++ webpack.config.dist.js
@@ -1,41 +1,33 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin')
 const path = require('path')
-const webpack = require('webpack')
 
 module.exports = {
-  devtool: 'eval',
+  devtool: 'source-map',
   entry: [
-    'babel/polyfill',
-    './website/index.js'
+    './source/index.js'
   ],
+  externals: {
+    immutable: {
+      commonjs: 'immutable',
+      commonjs2: 'immutable',
+      amd: 'immutable',
+      root: 'Immutable'
+    }
+  },
   output: {
-    path: 'build',
-    filename: '/static/[name].js'
+    path: 'dist',
+    filename: 'immutable-js-store.js',
+    libraryTarget: 'commonjs2',
+    library: 'immutable-js-store'
   },
   plugins: [
-    new HtmlWebpackPlugin({
-      filename: 'index.html',
-      inject: true,
-      template: './website/index.html'
-    }),
-    new webpack.NoErrorsPlugin()
   ],
   module: {
     loaders: [
       {
         test: /\.js$/,
         loader: 'babel',
-        exclude: path.join(__dirname, 'node_modules')
-      },
-      {
-        test: /\.css$/,
-        loaders: ['style', 'css?modules&importLoaders=1', 'cssnext'],
-        exclude: path.join(__dirname, 'node_modules')
+        include: path.join(__dirname, 'source')
       }
     ]
-  },
-  devServer: {
-    contentBase: 'build',
-    port: 3456
   }
 }
